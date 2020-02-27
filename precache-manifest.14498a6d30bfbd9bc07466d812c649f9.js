0d58c3af456ae36befdc8c377c042b85620a9fb8
Katie Hoyos <katie.rhoyos@gmail.com>
2020-02-26 19:33:47 -0800

    Updates

---
+++ precache-manifest.14498a6d30bfbd9bc07466d812c649f9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fdb07e91c1a66b7552e3591666c2f4ef",
+    "url": "/movies/index.html"
+  },
+  {
+    "revision": "7e4483c39952b6b7af3e",
+    "url": "/movies/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "54ea082bc4dccea4727e",
+    "url": "/movies/static/js/2.a348ed77.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/movies/static/js/2.a348ed77.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7e4483c39952b6b7af3e",
+    "url": "/movies/static/js/main.bc9df8f6.chunk.js"
+  },
+  {
+    "revision": "f3369c221346312a480f",
+    "url": "/movies/static/js/runtime-main.8de7a103.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/movies/static/media/logo.5d5d9eef.svg"
+  }
+]);
